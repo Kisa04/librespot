332eb4f63753ff7e6c0632e496474fe76ee9eb5d
Kisa <55666909+Kisa04@users.noreply.github.com>
2023-12-06 21:31:14 +0000

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import fs from 'fs'
-import Librespot from 'librespot'
+import Librespot from './index.js'
 
 const spotify = new Librespot()
 
-let a = await spotify.login("danielmendes004+spotify@gmail.com", "danny_jmendes")
+let a = spotify.login("danielmendes004+spotify@gmail.com", "danny_jmendes")
 
 console.log(a)
 
-const track = await spotify.get.track('1p80LdxRV74UKvL8gnD7ky')
+const track =  spotify.get.track('1p80LdxRV74UKvL8gnD7ky')
 
 console.log(track)
 
-await fs.promises.writeFile('example.ogg', track.stream)
+ fs.promises.writeFile('example.ogg', track.stream)
 
-await spotify.disconnect()
+ spotify.disconnect()
